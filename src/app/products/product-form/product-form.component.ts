a8ca86258eb0f054c15bbc1440bec5ba37847148
Conrado Pereira <conrado.spereira@gmail.com>
2022-10-13 21:18:47 -0300

    Using formbuilder.

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      description: [null],
+      code: [null],
+      price: [null],
+      amount: [null]
+    });
+  }
 
   ngOnInit(): void {
   }
