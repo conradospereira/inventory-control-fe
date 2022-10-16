99deb1758379d47ccc0e24267f08a71e25cbd212
Conrado Pereira <conrado.spereira@gmail.com>
2022-10-16 13:19:48 -0300

    Refactoring for the new Angular forms update v14.

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { ProductsService } from './../services/products.service';
 
 @Component({
   selector: 'app-product-form',
@@ -8,18 +12,38 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ProductFormComponent implements OnInit {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    description: [''],
+    code: [0],
+    price: [0],
+    amount: [0]
+  });
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      description: [null],
-      code: [null],
-      price: [null],
-      amount: [null]
-    });
+  constructor(private formBuilder: NonNullableFormBuilder,
+    private service: ProductsService,
+    private snackBar: MatSnackBar,
+    private location: Location) {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(result => this.onSucess(), error => this.onError());
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  private onSucess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 })
+    this.onCancel();
+  }
+
+  private onError() {
+    this.snackBar.open('Erro ao salvar curso!', '', { duration: 5000 });
   }
 
 }
